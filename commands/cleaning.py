1762e5c5eae240879025515c1da9facbb743c76c
antlord <antlord92@gmail.com>
2016-03-14 17:57:28 +0800

    cleaning phases has become by one SQL query.

--- commands/cleaning.py
+++ commands/cleaning.py
@@ -7,6 +7,10 @@ TABLENAME = Phase.__tablename__
 
 def clear_phases(start: int, stop: int):
     conn = engine.connect()
+    resonance_ids = []
     for resonance in get_resonances(start, stop):
-        conn.execute('DELETE FROM %s WHERE resonance_id=%i' % (TABLENAME, resonance.id))
         REDIS.delete('%s:%i' % (TABLENAME, resonance.id))
+        resonance_ids.append(str(resonance.id))
+
+    conn.execute("DELETE FROM %s WHERE resonance_id = ANY('{%s}'::int[]);" %
+                 (TABLENAME, ','.join(resonance_ids)))
