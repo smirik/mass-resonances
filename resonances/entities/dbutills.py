97dcca16af8717d8e91b47e4c8efedb95e914ffe
antlord <antlord92@gmail.com>
2017-01-11 04:23:38 +0800

    Updated modules Commands, Entities. Added test for insert query extension.
    Commands: Saving if broken asteroids has been make by upsert query.
    
    Entities: Added extension for insert queries adds "ON CONFLICT". It
    helps to meke upsert queries. Returning statement by default has been
    removed. Added type annotation PEP 484 for get_or_create method.

--- resonances/entities/dbutills.py
+++ resonances/entities/dbutills.py
@@ -2,9 +2,10 @@ import logging
 import os
 import time
 from typing import Tuple
-
+from typing import TypeVar
 import redis
 from alembic.config import Config as AlembicConfig
+from sqlalchemy.sql.expression import Executable
 from sqlalchemy import create_engine, Integer, Column
 from sqlalchemy import event
 from sqlalchemy.engine import Engine
@@ -12,6 +13,9 @@ from sqlalchemy.ext.declarative import as_declarative
 from sqlalchemy.orm import Session
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.sql import ClauseElement
+from sqlalchemy.sql import Insert
+from sqlalchemy.ext.compiler import compiles
+from sqlalchemy.dialects.postgresql.psycopg2 import PGCompiler_psycopg2
 
 from resonances.settings import Config
 
@@ -20,6 +24,7 @@ _HOST = CONFIG['postgres']['host']
 _USER = CONFIG['postgres']['user']
 _PASSWORD = CONFIG['postgres']['password']
 _DB = CONFIG['postgres']['db']
+T = TypeVar('T')
 
 if CONFIG['debug']:
     logging.basicConfig()
@@ -48,7 +53,8 @@ class Base(object):
         super(Base, self).__init__(**kwargs)
     id = Column(Integer, primary_key=True)
 
-engine = create_engine('postgres://%s:%s@%s/%s' % (_USER, _PASSWORD, _HOST, _DB))
+engine = create_engine('postgres://%s:%s@%s/%s' % (_USER, _PASSWORD, _HOST, _DB),
+                       implicit_returning=False)
 _Session = sessionmaker()
 _Session.configure(bind=engine)
 session = _Session()    # type: Session
@@ -60,7 +66,7 @@ _conn = redis.ConnectionPool(
 REDIS = redis.Redis(connection_pool=_conn)
 
 
-def get_or_create(cls: type, **kwargs):
+def get_or_create(cls: type, **kwargs) -> Tuple[T, bool]:
     """
     :param kwargs:
     :param type cls:
@@ -75,3 +81,23 @@ def get_or_create(cls: type, **kwargs):
         instance = cls(**params)
         session.add(instance)
         return instance, True
+
+
+class OnConflictInsert(Executable, ClauseElement):
+    def __init__(self, insert_expr: Insert, indexes=None):
+        self.insert = insert_expr
+        self._returning = None
+        self.table = insert_expr.table
+        self.indexes = indexes
+
+
+@compiles(OnConflictInsert)
+def on_conflict(elem: OnConflictInsert, compiler: PGCompiler_psycopg2, **kw):
+    query_string = compiler.visit_insert(elem.insert, **kw)  # type: str
+    action = 'ON CONFLICT %s DO NOTHING'
+    if elem.indexes:
+        fields = '(%s)' % ', '.join(elem.indexes)
+        action = action % fields
+    else:
+        action = action % ''
+    return '%s %s' % (query_string, action)
